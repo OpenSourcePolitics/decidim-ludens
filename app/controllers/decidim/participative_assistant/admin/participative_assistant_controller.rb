0fa402cf07c071a78f0ce39a7ae4eb561e34c054
eliegaboriau <elie@opensourcepolitics.eu>
2021-12-03 11:21:27 +0100

    Add modals

--- app/controllers/decidim/participative_assistant/admin/participative_assistant_controller.rb
+++ app/controllers/decidim/participative_assistant/admin/participative_assistant_controller.rb
@@ -3,14 +3,22 @@ module Decidim
     module Admin
       class ParticipativeAssistantController < ParticipativeAssistant::Admin::ApplicationController
         def show;
-          @recoEdition=ParticipativeAction.where(category:"Edition",completed:FALSE)
-          @recoEditionDone=ParticipativeAction.where(category:"Edition",completed:TRUE)
-          @recoConfig=ParticipativeAction.where(category:"Configuration",completed:FALSE)
-          @recoConfigDone=ParticipativeAction.where(category:"Configuration",completed:TRUE)
-          @recoCollab=ParticipativeAction.where(category:"Collaboration",completed:FALSE)
-          @recoCollabDone=ParticipativeAction.where(category:"Collaboration",completed:TRUE)
-          @recoInteraction=ParticipativeAction.where(category:"Interaction",completed:FALSE)
-          @recoInteractionDone=ParticipativeAction.where(category:"Interaction",completed:TRUE)
+          @recoEdition=ParticipativeAction.where(category:"Edition",completed:FALSE).limit(5)
+          @recoEditionTotal=ParticipativeAction.where(category:"Edition",completed:FALSE)
+          @recoEditionDone=ParticipativeAction.where(category:"Edition",completed:TRUE).limit(5)
+          @recoEditionDoneTotal=ParticipativeAction.where(category:"Edition",completed:TRUE)
+          @recoConfig=ParticipativeAction.where(category:"Configuration",completed:FALSE).limit(5)
+          @recoConfigTotal=ParticipativeAction.where(category:"Configuration",completed:FALSE)
+          @recoConfigDone=ParticipativeAction.where(category:"Configuration",completed:TRUE).limit(5)
+          @recoConfigDoneTotal=ParticipativeAction.where(category:"Configuration",completed:TRUE)
+          @recoCollab=ParticipativeAction.where(category:"Collaboration",completed:FALSE).limit(5)
+          @recoCollabTotal=ParticipativeAction.where(category:"Collaboration",completed:FALSE)
+          @recoCollabDone=ParticipativeAction.where(category:"Collaboration",completed:TRUE).limit(5)
+          @recoCollabDoneTotal=ParticipativeAction.where(category:"Collaboration",completed:TRUE)
+          @recoInteraction=ParticipativeAction.where(category:"Interaction",completed:FALSE).limit(5)
+          @recoInteractionTotal=ParticipativeAction.where(category:"Interaction",completed:FALSE)
+          @recoInteractionDone=ParticipativeAction.where(category:"Interaction",completed:TRUE).limit(5)
+          @recoInteractionDoneTotal=ParticipativeAction.where(category:"Interaction",completed:TRUE)
         end
       end
     end
