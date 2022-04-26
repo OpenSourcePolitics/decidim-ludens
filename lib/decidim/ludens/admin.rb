079ce8f4e78eca17fe298cb2f3a21a87bee4208f
eliegaboriau <93646702+eliegaboriau@users.noreply.github.com>
2022-04-26 12:09:14 +0200

    Update admin.rb

--- lib/decidim/ludens/admin.rb
+++ lib/decidim/ludens/admin.rb
@@ -2,7 +2,7 @@
 
 module Decidim
   module Ludens
-    # This module contains all the domain logic associated to Decidim's ParticipativeAssistant
+    # This module contains all the domain logic associated to Decidim's Ludens
     # component admin panel.
     module Admin
     end
