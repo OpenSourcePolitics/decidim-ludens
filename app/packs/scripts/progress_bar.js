a5ce8e48d11264b33bda666cc98d6fef3e624a12
eliegaboriau <elie@opensourcepolitics.eu>
2022-05-20 14:11:17 +0200

    fix progress bar

--- app/packs/scripts/progress_bar.js
+++ app/packs/scripts/progress_bar.js
@@ -4,7 +4,7 @@ $paliersScore=$paliersScore.substr(1,$paliersScore.length-2).split(',');
 $progressSize=window.getComputedStyle(document.querySelector(".progress")).getPropertyValue("width");
 $progressSize=Math.round($progressSize.substring(0,$progressSize.length-2)/ [document.documentElement.clientWidth]*100);
 if($score<$paliersScore[0]){
-    $cssValue=$score/$paliersScore[0]*7;
+    $cssValue=$score/$paliersScore[0]*$progressSize;
 }
 else if($score<$paliersScore[1]){
     $cssValue=($score-$paliersScore[0])/($paliersScore[1]-$paliersScore[0])*$progressSize;
@@ -19,6 +19,6 @@ else if($score<$paliersScore[4]){
     $cssValue=($score-$paliersScore[3])/($paliersScore[4]-$paliersScore[3])*$progressSize;
 }
 else{
-    $cssValue=7;
+    $cssValue=$progressSize;
 }
 document.querySelector(".progress_bar").style.width = $cssValue.toString()+"vw";
