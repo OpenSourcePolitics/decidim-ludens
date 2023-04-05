b42e31b30374fa89057733b09c726a30837421a4
eliegaboriau <93646702+eliegaboriau@users.noreply.github.com>
2023-04-05 12:14:04 +0200

    fix level up (#51)

--- spec/services/manage_points_service_spec.rb
+++ spec/services/manage_points_service_spec.rb
@@ -48,6 +48,27 @@ module Decidim
             expect(Rails.cache.read("flash_message_#{user.id}")).to eq("Congratulations ! You just completed the action 'Create a participatory process' !")
           end
         end
+
+        context "when participative action is present and makes you level up" do
+          let!(:resource) { create :participatory_process, organization: organization }
+          let!(:cache_store) { :memory_store }
+          let!(:pac1) { create(:participative_action_completed, user: user, decidim_participative_action: "create.Decidim::Blogs::Post") }
+          let!(:pac2) { create(:participative_action_completed, user: user, decidim_participative_action: "update.Decidim::Meetings::Agenda") }
+          let!(:pac3) { create(:participative_action_completed, user: user, decidim_participative_action: "update.Decidim::Debates::Debate") }
+          let!(:pac4) { create(:participative_action_completed, user: user, decidim_participative_action: "deliver.Decidim::Newsletter") }
+
+          before do
+            allow(Rails).to receive(:cache).and_return(ActiveSupport::Cache.lookup_store(cache_store))
+            Rails.cache.clear
+          end
+
+          it "completes organization and saves the level up" do
+            subject_run
+            expect(ParticipativeActionCompleted.last.participative_action.global_id).to eq("create.Decidim::ParticipatoryProcess")
+            expect(Rails.cache.read("flash_message_#{user.id}")).to eq("Congratulations ! You just completed the action 'Create a participatory process' !")
+            expect(Rails.cache.read("level_up_#{user.id}")).to eq("reached")
+          end
+        end
       end
     end
   end
